c3aa18ae11099c413803638b39bfad2b863b7a28
Daniel Solano <daniel-9122@hotmail.com>
2018-04-03 16:34:33 -0600

    print ln  prueba
    println prueba

--- src/main/java/cr/ac/uca/paraiso/lenguajes/DemoAppApplication.java
+++ src/main/java/cr/ac/uca/paraiso/lenguajes/DemoAppApplication.java
@@ -10,7 +10,8 @@ public class DemoAppApplication {
 		SpringApplication.run(DemoAppApplication.class, args);
                 
                 System.out.println("lalalalal");
-                
+                System.out.println("subir update del repo");
+            
                 
                 System.out.println("Valida los datos de estudiante");
 	}
